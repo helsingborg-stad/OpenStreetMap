a1f8949f56f9873252f8c31fbcff0a22f3632aef
NiclasNorin <niclasn@outlook.com>
2025-02-19 10:37:50 +0100

    feat: create marker

--- js/features/createMarker/createMarker.ts
+++ js/features/createMarker/createMarker.ts
@@ -1,23 +1,29 @@
-import L, { Map as LeafletMap } from 'leaflet';
+import L, { Marker } from 'leaflet';
 import { MapInterface } from '../../mapInterface';
-import { CreateMarkerInterface, MarkerOptions } from './createMarkerInterface';
+import { CreateMarkerInterface, MarkerOptions, TooltipOptions } from './createMarkerInterface';
 
-class CreateMarker implements CreateMarkerInterface {
+export class CreateMarker implements CreateMarkerInterface {
     constructor(private mapInstance: MapInterface) {
     }
 
-    public create(markerOptions: MarkerOptions): void {
-        let marker = L.divIcon({
+    public create(markerOptions: MarkerOptions, tooltipOptions: TooltipOptions|null = null): Marker {
+        const icon = L.divIcon({
             className: markerOptions.className ?? '',
             html: markerOptions.icon, 
         });
 
-        if (markerOptions.tooltip) {
-            marker.bindPopup(markerOptions.tooltip.content, {
-                closeButton: markerOptions.tooltip.closeButton ?? true,
-                maxHeight: markerOptions.tooltip.maxHeight ?? 250,
-                maxWidth: markerOptions.tooltip.maxWidth ?? 100,
+        const marker = L.marker(markerOptions.position, {
+            icon: icon
+        });
+
+        if (tooltipOptions) {
+            marker.bindPopup(tooltipOptions.content, {
+                closeButton: tooltipOptions.closeButton ?? true,
+                maxHeight: tooltipOptions.maxHeight ?? 250,
+                maxWidth: tooltipOptions.maxWidth ?? 100,
             });
         }
+
+        return marker;
     }
 }
