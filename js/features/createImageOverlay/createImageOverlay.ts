f2250f0863ee51d9f268905db9a7dd3614df97e3
NiclasNorin <niclasn@outlook.com>
2025-02-28 10:51:53 +0100

    fix: adding image overlay options

--- js/features/createImageOverlay/createImageOverlay.ts
+++ js/features/createImageOverlay/createImageOverlay.ts
@@ -1,17 +1,22 @@
 import { LatLngBoundsObject } from "../../types";
-import { CreateImageOverlayInterface } from "./createImageOverlayInterface";
+import { CreateImageOverlayInterface, ImageOverlayOptions } from "./createImageOverlayInterface";
 import { ImageOverlay } from "./imageOverlay";
 import { ImageOverlayInterface } from "./imageOverlayInterface";
 import L from 'leaflet';
 
 export class CreateImageOverlay implements CreateImageOverlayInterface {
-    public create(url: string, latLngBounds: LatLngBoundsObject): ImageOverlayInterface {
+    public create(
+        url: string,
+        latLngBounds: LatLngBoundsObject,
+        imageOverlayOptions: ImageOverlayOptions = {}
+    ): ImageOverlayInterface {
         const imageOverlay = L.imageOverlay(
             url, 
             [
                 [latLngBounds.southWest.lat, latLngBounds.southWest.lng], 
                 [latLngBounds.northEast.lat, latLngBounds.northEast.lng]
-            ]
+            ],
+            imageOverlayOptions
         );
 
         return new ImageOverlay(imageOverlay);
--- js/features/createImageOverlay/createImageOverlayInterface.ts
+++ js/features/createImageOverlay/createImageOverlayInterface.ts
@@ -1,6 +1,12 @@
 import { LatLngBoundsObject } from "../../types";
 import { ImageOverlayInterface } from "./imageOverlayInterface";
 
+export type ImageOverlayOptions =  {
+    opacity?: number;
+    alt?: string;
+    className?: string;
+}
+
 export interface CreateImageOverlayInterface {
-    create(url: string, latLngBounds: LatLngBoundsObject): ImageOverlayInterface;
+    create(url: string, latLngBounds: LatLngBoundsObject, imageOverlayOptions: ImageOverlayOptions): ImageOverlayInterface;
 }
