5561fb127c7f059e7bfa47385f75a0f5a33d4ab1
NiclasNorin <niclasn@outlook.com>
2025-03-04 15:31:11 +0100

    refactor: structured export

--- js/index.ts
+++ js/index.ts
@@ -15,12 +15,22 @@ export * from './setupMap/createMapInterface';
 export * from './setupMap/config/configInterface';
 
 // Features
-export { CreateMarker } from './features/createMarker/createMarker';
 export { CreateLayerGroup } from './features/createLayerGroup/createLayerGroup';
-export { CreateImageOverlay } from './features/createImageOverlay/createImageOverlay';
-export { CreateRectangle } from './features/createRectangle/createRectangle';
+export { LayerGroup } from './features/createLayerGroup/layerGroup';
 export * from './features/createLayerGroup/createLayerGroupInterface';
-export * from './features/createMarkerClusterGroup/createMarkerClusterGroupInterface';
-export * from './features/createImageOverlay/createImageOverlayInterface';
-export * from './features/createMarker/createMarkerInterface';
+export * from './features/createLayerGroup/layerGroupInterface';
+
+export { CreateRectangle } from './features/createRectangle/createRectangle';
+export { Rectangle } from './features/createRectangle/rectangle';
 export * from './features/createRectangle/createRectangleInterface';
+export * from './features/createRectangle/rectangleInterface';
+
+export { CreateMarker } from './features/createMarker/createMarker';
+export { Marker } from './features/createMarker/marker';
+export * from './features/createMarker/createMarkerInterface';
+export * from './features/createMarker/markerInterface';
+
+export { CreateImageOverlay } from './features/createImageOverlay/createImageOverlay';
+export { ImageOverlay } from './features/createImageOverlay/imageOverlay';
+export * from './features/createImageOverlay/createImageOverlayInterface';
+export * from './features/createImageOverlay/imageOverlayInterface';
